7b78befa487e0cdccbbd6dd1fbbabd84e6c8f0a8
Mateusz Nosel <meffiunosel@gmail.com>
2025-04-08 14:32:35 +0200

    Notification API Created

---
+++ NotificationApp.Api/Controllers/NotificationsController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NotificationApp.Application.Notifications.Commands;
+
+namespace NotificationApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public NotificationsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateNotificationCommand command)
+        {
+            var id = await _mediator.Send(command);
+            return CreatedAtAction(nameof(GetById), new { id }, null);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            // TODO: implementacja później
+            return Ok(new { Id = id });
+        }
+    }
+}
--- NotificationApp.Domain/Enums/NotificationChannel.cs
+++ NotificationApp.Domain/Enums/NotificationChannel.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace NotificationApp.Domain.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum NotificationChannel
     {
-        push,
-        Email
+        Email,Push
     }
 }
